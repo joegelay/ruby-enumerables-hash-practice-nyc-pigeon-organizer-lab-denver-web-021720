0afbb0bffbdc24e27ad6a5fb977d863822d6bb4d
Joe Gelay <joegelay@gmail.com>
2020-01-30 17:40:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,21 @@
 def nyc_pigeon_organizer(data)
   result = {}
-  
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !result[name]
+          result[name] = {}
+        end
+
+        if !result[name][key]
+          result[name][key] = []
+        end
+
+        result[name][key] << new_value.to_s
+
+      end
+    end
+  end
   return result
 end
